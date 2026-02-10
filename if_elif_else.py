aa8e43ec5828411e7ce54ffe6f8a77c93950b21d
Atharv Singh <atharvsin03@gmail.com>
2026-02-10 18:07:16 +0530

    Updated variable.py

---
+++ if_elif_else.py
@@ -0,0 +1,22 @@
+# if examples
+# elif examples
+#1.
+dayin = int(input("Enter the day as a number (e.g. 1): "))
+if dayin == 1:
+  print("Today is Monday")
+elif dayin == 2:
+  print("Today is Tuesday")
+elif dayin == 3:
+  print("Today is Wednesday")
+elif dayin == 4:
+  print("Today is Thursday")
+elif dayin == 5:
+  print("Today is Friday")
+elif dayin == 6:
+  print("Today is Saturday")
+elif dayin == 7:
+  print("Today is Sunday")
+elif dayin == 67:
+  print("Today's day is in the weekend")
+else:
+  print("Wrong digit")
--- variable.py
+++ variable.py
@@ -47,29 +47,191 @@ price = int(input("Enter price: "))
 gst = price * 0.18
 print("The GST amount of" ,price, "is" ,gst)
 # 10. Area of Circle: π * r * r
-r = int(input("Enter raduis of circle: "))
+cir = int(input("Enter raduis of circle: "))
 pi = 3.14159265359
-area = pi * r * r
+area = pi * cir * cir
 print("The area of the circle is:" ,area)
 
 #11. Compound Interest: A = P (1 + r/100)^t
-#12. BMI Calculator: weight / (height * height)
+p = int(input("Enter Principal amount: "))
+t = int(input("Enter time amount in years (e.g. 2): "))
+cr = int(8.5)
+a = p*(1 + cr/100)**t
+print(p, "with an intrest of 20 percentage for" ,t, "years becomes:" ,a)
+# BMI Calculator using height in cm
+weight = float(input("Enter Weight (in kg): "))
+height_cm = float(input("Enter Height (in cm): "))
+age = int(input("Enter Age (in years): "))
+height_m = height_cm / 100
+bmi = weight / (height_m ** 2)
+bmi = round(bmi, 2)
+
+print("\nYour BMI is:", bmi)
+if age >= 18:
+  if bmi < 18.5:
+    print("Because you are 18 or above, you are currently underweight")
+  elif 18.5 <= bmi < 25:
+    print("Because you are 18 or above, you are currently normal weight")
+  elif 25 <= bmi < 30:
+    print("Because you are 18 or above, you are currently overweight")
+  else:
+    print("Because you are 18 or above, you are currently obese")
+else:
+  if bmi < 14:
+    print("Because you are under 18, you are currently underweight")
+  elif 14 <= bmi < 20:
+    print("Because you are under 18, you are currently normal weight")
+  elif 20 <= bmi < 25:
+    print("Because you are under 18, you are currently overweight")
+  else:
+    print("Because you are under 18, you are currently obese")
+
+print("\nNote: For kids & teens, real BMI uses age-percentile charts.")
 #13. Total Salary = Basic + HRA(20%) + DA(40%)
+b = int(input("\nEnter Basic Amount (in ₹): "))
+hra = 0.2
+da = 0.4
+hrda = (hra + da)*b
+salary = b + hrda
+print("Your salary is:" ,salary, "\n")
 #14. Calculate Discount on a Product
+discount_per = int(input("Enter Discount (in %): "))
+price = int(input("Enter orignal price: "))
+discount_amo = (discount_per / 100) * price
+newprice = price - discount_amo
+print("The discount amount is:" ,discount_amo, "and the new price is: ₹" ,newprice, "\n")
 #15. Fahrenheit to Celsius: C = (F − 32) * 5/9
+fh = int(input("Enter Fahrenheit: "))
+cl = (fh - 32) * (5 / 9)
+print(fh,"F in celsius is" ,cl,"C")
 #16. Electricity Bill Calculator (slab wise)
 #17. Calculate Speed = distance / time
+distance = int(input("Enter Distance: "))
+time = int(input("Enter Time taken: "))
+speed = distance / time
+print("If the distance is" ,distance, "and the time taken is" ,time, "then the speed is" ,speed, "\n")
 #18. Grading System based on average marks
+mathmar = int(input("Enter number of marks in Math: "))
+sciencemar = int(input("Enter number of marks in Science: "))
+comuptermar = int(input("Enter number of marks in Computer: "))
+biomar = int(input("Enter number of marks in Biology: "))
+avermar = (mathmar + sciencemar + comuptermar + biomar) / 2
+print("Your average marks are" ,avermar)
+if (avermar >= 90):
+  print("You got grade A!")
+elif (avermar >= 70):
+  print("You got grade B!")
+elif (avermar >= 50):
+  print("You got grade C")
+elif (avermar < 50):
+  print("You got grade D")
+elif (avermar < 30):
+  print("You got grade F")
 #19. Convert Days to Years, Months, Days
+days = int(input("Enter number of days"))
+leap = input("Is it leap year? Answer in Yes or No: ")
+if (leap == "Yes"):
+  years = days / 366
+  months = days / 31
+  print("The number of years would be:" ,years, " and it would be:" ,months,"months")
+elif (leap == "No"):
+  years = days / 365
+  months = days / 31
+  print("The number of years would be:" ,years, " and it would be:" ,months,"months")
+else:
+  print("You have to either type 'Yes' or 'No not" ,leap)
 #20. SI with different rates for each year
+sip = float(input("Enter Principal: ₹"))
+years = int(input("Enter number of years: "))
+total_rate = 0
+
+for i in range(years):
+    rate = float(input(f"Enter rate for year {i+1}: "))
+    total_rate += rate
+
+total_si = (sip * total_rate) / 100
+total_amount = sip + total_si
+
+print("Total Interest: ₹", total_si)
+print("Total Amount to pay: ₹", total_amount)
 
 #21. EMI Calculator
+emip = 100000
+emiyr = 10
+emitm = 12
+
+monthly_rate = emiyr / (12 * 100)
+emi = (emip * monthly_rate * (1 + monthly_rate)**emitm) / ((1 + monthly_rate)**emitm - 1)
+print(round(emi, 2))
 #22. Profit & Loss % Calculator
+cp = int(input("Enter Cost Price: "))
+sp = int(input("Enter Sell Price: "))
+if (sp > cp):
+  p = sp - cp
+  print("There is a profit of ", p)
+elif (cp > sp):
+  l = cp - sp
+  print("There is a loss of ", l)
 #23. Distance Between Two Points
+v1x = int(input("Enter Point 1 X: "))
+v1y = int(input("Enter Point 1 Y: "))
+v2x = int(input("Enter Point 2 X: "))
+v2y = int(input("Enter Point 2 Y: "))
+ys = (v2y - v1y)*2
+xs = (v2x - v1x)*2
+d = (ys + xs)**2
+print("The distance is:" ,d)
 #24. Loan Eligibility Checker
 #25. Percentage Calculator for 5 Subjects
+amaths = int(input("Enter marks in Maths: "))
+ascience = int(input("Enter marks in Science: "))
+asst = int(input("Enter marks in SST: "))
+aenglish = int(input("Enter marks in English: "))
+acomputer = int(input("Enter marks in Computer: "))
+total_amount_per = amaths + ascience + asst + aenglish + acomputer
+pmaths = (amaths/total_amount_per) * 100
+pscience = (ascience/total_amount_per) * 100
+psst = (asst/total_amount_per) * 100
+penglish = (aenglish/total_amount_per) * 100
+pcomputer = (acomputer/total_amount_per) * 100
+print("The percantage of Maths is:" ,pmaths, "% and in Science:" ,pscience, "% and in SST:" ,psst, "% and in English:" ,penglish, "% and in Computer:" ,pcomputer)
 #26. Volume of Cylinder: π * r2 * h
+cyraduis = int(input("Enter Raduis: "))
+cyheight = int(input("Enter Height: "))
+cyvolume = pi * (cyraduis * 2) * cyheight
+print("The volume of the cylinder is" ,cyvolume)
 #27. Currency Converter (INR to USD/EUR)
+inr = int(input("Enter amount of money in INR: ₹"))
+usdreur = input("USD (USA dollar), EUR (Euro) or both?: ")
+if (usdreur == "USD"):
+  usd = inr * 0.011
+  print("In USD (USA dollar)" ,inr, "rupees would be $" ,usd)
+elif (usdreur == "EUR"):
+  eur = inr * 0.0093
+  print("In EUR (Euro)" ,inr, "rupees would be €" ,eur)
+elif (usdreur == "Both" or usdreur == "both"):
+  usd = inr * 0.011
+  eur = inr * 0.0093
+  print("In USD (USA dollar)" ,inr, "rupees would be $" ,usd, "and in EUR (Euro) it would be €" ,eur)
 #28. GST Split: CGST + SGST
 #29. CAGR: (End/Start)^(1/n) - 1
+start_val = 100000
+end_val = 150000
+years = 3
+cagr = (end_val / start_val)**(1 / years) - 1
+finalcagr =  {round(cagr * 100, 2)}%
+print("CAGR is:" ,finalcagr)
 #30. Working Hours Salary with Overtime
+base_salary = 40000
+standard_hours = 160
+totalhrsw = 175
+ot_multiplier = 2
+
+hourly_rate = base_salary / standard_hours
+overtime_hours = max(0, totalhrsw - standard_hours)
+
+regular_pay = standard_hours * hourly_rate
+overtime_pay = overtime_hours * (hourly_rate * ot_multiplier)
+total_salary = regular_pay + overtime_pay
+
+print("The total salary is:" ,total_salary)
